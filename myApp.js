63cea72a519b528020820efd3391c20218d82b1f
Joey3011 <jeff.me.7919@gmail.com>
2022-11-09 20:14:35 +0800

    Chain Search Query Helpers to Narrow Search Results

--- myApp.js
+++ myApp.js
@@ -94,8 +94,10 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort(name).limit(2).select(-age).exec((err, findResult) => {
+    if(err) return console.log(err)
+     done(null , findResult);
+  })
 };
 
 /** **Well Done !!**
